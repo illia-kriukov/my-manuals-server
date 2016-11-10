94fee8ca43a6cafe7243c55d09fa055d759e855b
Ilya Kriukov <ilyakriukov@icloud.com>
2016-11-10 21:20:55 +0100

    MYMNLS-1003: Simple Hello World example

---
+++ src/main/java/dv611/mymanuals/Application.java
@@ -0,0 +1,16 @@
+package dv611.mymanuals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ilyakruikov on 11/10/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
