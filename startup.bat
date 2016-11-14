7c12853933a2522ec19af71665aa037e44d09c07
Ilya Kriukov <ilyakriukov@icloud.com>
2016-11-14 13:12:21 +0100

    MYMNLS-1003: Add startup files for Unix (.sh) and Windows (.bat)

---
+++ startup.bat
@@ -0,0 +1,3 @@
+@echo off
+mvn clean install -P dev
+java -jar target/4DV611.mymanuals-1.0-SNAPSHOT.jar
---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mvn clean install -P dev
+java -jar target/4DV611.mymanuals-1.0-SNAPSHOT.jar
