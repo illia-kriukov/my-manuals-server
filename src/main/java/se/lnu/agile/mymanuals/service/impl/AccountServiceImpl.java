12a2dd2e399955f479196588bbd31431a4704507
Panagiotis Kazilas <kpanayiotis@hotmail.gr>
2016-11-14 10:42:43 +0100

    Evaluation of representative at Sign-Up

--- src/main/java/se/lnu/agile/mymanuals/service/impl/AccountServiceImpl.java
+++ src/main/java/se/lnu/agile/mymanuals/service/impl/AccountServiceImpl.java
@@ -76,7 +76,7 @@ public class AccountServiceImpl implements AccountService {
 
     private boolean checkEmailFormat(String email){
         String EMAIL_PATTERN =   "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                               + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
