149fed548d0fcda4fee392d37ec757efb6d18850
Ilya Kriukov <ilyakriukov@icloud.com>
2016-11-25 12:37:20 +0100

    MYMNLS-984: Get access_token for consumer (Consumer Log In)

--- src/main/java/se/lnu/agile/mymanuals/security/MyManualsUserDetailsService.java
+++ src/main/java/se/lnu/agile/mymanuals/security/MyManualsUserDetailsService.java
@@ -10,26 +10,77 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import se.lnu.agile.mymanuals.dao.ConsumerDao;
 import se.lnu.agile.mymanuals.dao.RepresentativeDao;
+import se.lnu.agile.mymanuals.model.Consumer;
 import se.lnu.agile.mymanuals.model.Representative;
 
 @Service
 public class MyManualsUserDetailsService implements UserDetailsService {
 
+	private final ConsumerDao consumerDao;
+
 	private final RepresentativeDao representativeDao;
 
 	@Autowired
-	public MyManualsUserDetailsService(RepresentativeDao userRepository) {
-		this.representativeDao = userRepository;
+	public MyManualsUserDetailsService(ConsumerDao consumerDao, RepresentativeDao representativeDao) {
+		this.consumerDao = consumerDao;
+		this.representativeDao = representativeDao;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Representative representative = representativeDao.findByEmail(username);
-		if (representative == null) {
-			throw new UsernameNotFoundException(String.format("User %s does not exist!", username));
+		Consumer consumer = consumerDao.findByEmail(username);
+		if (consumer != null) {
+			return new ConsumerDetails(consumer);
+		} else {
+			Representative representative = representativeDao.findByEmail(username);
+			if (representative != null) {
+				return new RepresentativeDetails(representative);
+			} else {
+				throw new UsernameNotFoundException(String.format("User %s does not exist!", username));
+			}
 		}
-		return new RepresentativeDetails(representative);
+	}
+
+	private final static class ConsumerDetails extends Consumer implements UserDetails {
+
+		private static final long serialVersionUID = 1L;
+
+		private ConsumerDetails(Consumer consumer) {
+			super(consumer);
+		}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+		}
+
+		@Override
+		public String getUsername() {
+			return getEmail();
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			return true;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			return true;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			return true;
+		}
+
+		@Override
+		public boolean isEnabled() {
+			return true;
+		}
+
 	}
 
 	private final static class RepresentativeDetails extends Representative implements UserDetails {
