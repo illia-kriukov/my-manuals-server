a80d6ef831e9fe1caa63d65f5d5c898b96e24272
Ilya Kriukov <ilyakriukov@icloud.com>
2016-11-30 11:44:44 +0100

    MYMNLS-986: Improve Category model

--- src/main/java/se/lnu/agile/mymanuals/model/Category.java
+++ src/main/java/se/lnu/agile/mymanuals/model/Category.java
@@ -1,9 +1,7 @@
 package se.lnu.agile.mymanuals.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by Daniel on 17.11.2016.
@@ -17,6 +15,9 @@ public class Category {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "category")
+    private List<Product> product;
 
     public Category(String name) {
         this.name = name;
@@ -39,6 +40,14 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(List<Product> product) {
+        this.product = product;
     }
 
 }
